8f042b61e805482128295150e3c4c4fc8d940686
Trevor Landau <landau@users.noreply.github.com>
2023-11-06 10:29:35 -0500

    update

--- osx-install.sh
+++ osx-install.sh
@@ -9,6 +9,15 @@ pushd $DEV_TOOLS_DIR
 ############################################
 # --- Allow keyrepeat
 #defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+# For VS Code
+#defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false              
+# For VS Code Insider
+#defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false      
+# For VS Codium
+#defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false         
+# For VS Codium Exploration users
+#defaults write com.microsoft.VSCodeExploration ApplePressAndHoldEnabled -bool false   
+#defaults delete -g ApplePressAndHoldEnabled
 ############################################
 
 ############################################
